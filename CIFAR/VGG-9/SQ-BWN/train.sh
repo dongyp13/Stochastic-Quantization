f9b3b1e6f5dd7f81896962dafdae6d38c33edd87
dongyp13 <dongyp13@mails.tsinghua.edu.cn>
2017-07-16 15:51:27 +0800

    CIFAR_VGG9

---
+++ CIFAR/VGG-9/SQ-BWN/train.sh
@@ -0,0 +1,24 @@
+BASE_PATH=$(pwd)
+TRAIN_PATH=$(dirname $0)
+
+cd $TRAIN_PATH
+mkdir model
+
+TOOLS=$BASE_PATH/caffe/build/tools
+
+$TOOLS/caffe train \
+    --solver=solver_s1.prototxt -gpu 0
+
+$TOOLS/caffe train \
+    --solver=solver_s2.prototxt -gpu 0 \
+	--weights=model/cifar_vgg9_SQBWN_s1_iter_100000.caffemodel
+
+$TOOLS/caffe train \
+    --solver=solver_s3.prototxt -gpu 0 \
+	--weights=model/cifar_vgg9_SQBWN_s2_iter_100000.caffemodel
+
+$TOOLS/caffe train \
+    --solver=solver_s4.prototxt -gpu 0 \
+	--weights=model/cifar_vgg9_SQBWN_s3_iter_100000.caffemodel
+
+
